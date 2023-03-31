865a8b5a2ddbb3113f29f6ad09689b2318bff81c
aozp73 <aozp73@naver.com>
2023-03-31 12:04:02 +0900

    pageview 학습 완료

---
+++ android/app/src/main/kotlin/shop/mtcoding/pageview_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package shop.mtcoding.pageview_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return Image.network("https://picsum.photos/id/${index+1}/200/300", fit: BoxFit.cover,);
+        },
+      ),
+    );
+  }
+}
